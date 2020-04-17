9b5bfc36e8d325855a791416bb9c31a721435247
au <auzhouqingyu@163.com>
2020-04-17 17:46:13 +0800

    leetcode-study ListCommonUtil 0021 mergeTwoLists

--- algorithms/src/main/java/top/auzqy/_0001_0050/_0021/Solution_0021.java
+++ algorithms/src/main/java/top/auzqy/_0001_0050/_0021/Solution_0021.java
@@ -12,7 +12,7 @@ package top.auzqy._0001_0050._0021;
  * 输入：1->2->4, 1->3->4
  * 输出：1->1->2->3->4->4
  *
- * createTime: 2020/4/17 14:38
+ * createTime: 2020/4/17 17:33
  * @author au
  */
 public class Solution_0021 {
@@ -26,8 +26,33 @@ public class Solution_0021 {
      * }
      */
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode hummyHead = new ListNode(0);
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
 
-        return null;
+        ListNode curr = hummyHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                curr.next = new ListNode(l1.val);
+                l1 = l1.next;
+            } else {
+                curr.next = new ListNode(l2.val);
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+
+        if (l1 == null) {
+            curr.next = l2;
+        }else {
+            curr.next = l1;
+        }
+
+        return hummyHead.next;
     }
 
 }
--- algorithms/src/test/java/top/auzqy/_0001_0050/_0021/Solution_0021Test.java
+++ algorithms/src/test/java/top/auzqy/_0001_0050/_0021/Solution_0021Test.java
@@ -1,26 +1,39 @@
 package top.auzqy._0001_0050._0021;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import top.auzqy.common.ListCommonUtil;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class Solution_0021Test {
+
+    private ListCommonUtil listCommonUtil = new ListCommonUtil();
     
-    static Stream<Arguments> expectedBoolean_inputListNode1_inputListNode2() {
+    static Stream<Arguments> expectedBoolean_expectedListNodeRest_inputListNode1_inputListNode2() {
         return Stream.of(
-                Arguments.of(),
-                Arguments.of()
+                Arguments.of(true,new int[]{1,1,2,3,4,4},new int[]{1,2,4},new int[]{1,3,4})
         );
     }
     
     @ParameterizedTest
-    @MethodSource("expectedBoolean_inputListNode1_inputListNode2")
-    void should_() {
-        
+    @MethodSource("expectedBoolean_expectedListNodeRest_inputListNode1_inputListNode2")
+    void should_(boolean expectedBoolean,
+                 int[] expectedListNodeRest,
+                 int[] inputListNode1,
+                 int[] inputListNode2) {
+        ListNode listNodeRest = listCommonUtil.buildListNode(expectedListNodeRest);
+
+        ListNode listNode1 = listCommonUtil.buildListNode(inputListNode1);
+        ListNode listNode2 = listCommonUtil.buildListNode(inputListNode2);
+
+        Solution_0021 solution0021 = new Solution_0021();
+        ListNode listNode = solution0021.mergeTwoLists(listNode1, listNode2);
+
+        Assertions.assertEquals(expectedBoolean,
+                listCommonUtil.compare(listNodeRest,listNode));
     }
 
 }
