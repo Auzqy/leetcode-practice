514b6f80d1d521c27bc5800a556612e2d9feb713
au <auzhouqingyu@163.com>
2020-07-21 11:03:13 +0800

    leetcode 226 pass

---
+++ algorithms/src/main/java/top/auzqy/_0201_0250/_0226/Solution_0226.java
@@ -0,0 +1,60 @@
+package top.auzqy._0201_0250._0226;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution_0226 {
+    public TreeNode invertTree(TreeNode root) {
+
+        return solution1(root);
+    }
+
+    private TreeNode solution1(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+    private TreeNode solution2(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode poll = queue.poll();
+
+            TreeNode tmp = poll.left;
+            poll.left = poll.right;
+            poll.right = tmp;
+
+            if (poll.left != null) {
+                queue.add(poll.left);
+            }
+            if (poll.right != null) {
+                queue.add(poll.right);
+            }
+        }
+
+        return root;
+    }
+
+
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
---
+++ algorithms/src/test/java/top/auzqy/_0201_0250/_0226/Solution_0226Test.java
@@ -0,0 +1,12 @@
+package top.auzqy._0201_0250._0226;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution_0226Test {
+
+    @Test
+    void invertTree() {
+    }
+}
