4f76ac3c0126379017ca3d95b446a3badfedccef
au <auzhouqingyu@163.com>
2020-07-22 23:16:55 +0800

    leetcode 0191 solution2 test pass

--- algorithms/src/main/java/top/auzqy/_0151_0200/_0191/Solution_0191.java
+++ algorithms/src/main/java/top/auzqy/_0151_0200/_0191/Solution_0191.java
@@ -8,7 +8,8 @@ package top.auzqy._0151_0200._0191;
 public class Solution_0191 {
 
     public int hammingWeight(int n) {
-        return solution1(n);
+//        return solution1(n);
+        return solution2(n);
     }
 
     private int solution1(int n) {
@@ -16,6 +17,17 @@ public class Solution_0191 {
         while(n != 0){
             n &= (n-1);
             count++;
+        }
+        return count;
+    }
+
+    private int solution2(int n) {
+        int mask =1, count = 0;
+        while (mask != 0) {
+            if ((mask & n) != 0) {
+                count++;
+            }
+            mask <<= 1;
         }
         return count;
     }
