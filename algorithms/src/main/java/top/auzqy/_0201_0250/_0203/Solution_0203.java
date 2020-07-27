b90901823e07134211cb1fe49efa222ad7917391
au <auzhouqingyu@163.com>
2020-07-27 09:49:57 +0800

    leetcode 0203 pass

---
+++ algorithms/src/main/java/top/auzqy/_0201_0250/_0203/Solution_0203.java
@@ -0,0 +1,30 @@
+package top.auzqy._0201_0250._0203;
+
+/**
+ * description:  https://leetcode-cn.com/problems/invert-binary-tree/
+ * createTime: 2020/7/21 11:19
+ * @author au
+ */
+public class Solution_0203 {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode p = dummy;
+        while (p.next != null) {
+            if (p.next.val == val) {
+                p.next = p.next.next;
+            } else {
+                p = p.next;
+            }
+        }
+
+        return dummy.next;
+    }
+}
+
