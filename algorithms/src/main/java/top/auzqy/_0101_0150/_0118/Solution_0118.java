938278db1b31c61264d48e35364184592a0e32e1
au <auzhouqingyu@163.com>
2020-08-21 23:57:20 +0800

    leetcode 118 test pass

---
+++ algorithms/src/main/java/top/auzqy/_0101_0150/_0118/Solution_0118.java
@@ -0,0 +1,50 @@
+package top.auzqy._0101_0150._0118;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * description:  https://leetcode-cn.com/problems/pascals-triangle/
+ * createTime: 2020/8/21 23:35
+ * @author au
+ */
+public class Solution_0118 {
+    public List<List<Integer>> generate(int numRows) {
+        return solution1_by_au(numRows);
+    }
+
+    private List<List<Integer>> solution1_by_au(int numRows) {
+        List<List<Integer>> res = new ArrayList<>(numRows);
+
+        // 用于存放上一个 list 元素的
+        List<Integer> lastTmpList = new LinkedList<>();
+
+        for (int i = 1; i <= numRows; i++) {
+            List<Integer> innerList = new ArrayList<>(i);
+            if (i == 1) {
+                innerList.add(1);
+            } else if (i == 2) {
+                innerList.add(1);
+                innerList.add(1);
+                // 重新记录
+                lastTmpList.clear();
+                lastTmpList.addAll(innerList);
+            } else {
+                innerList.add(1);
+                for (int j = 1; j < lastTmpList.size(); j++) {
+                    innerList.add(lastTmpList.get(j - 1) + lastTmpList.get(j));
+                }
+                innerList.add(1);
+
+                // 重新记录
+                lastTmpList.clear();
+                lastTmpList.addAll(innerList);
+            }
+            res.add(innerList);
+        }
+
+
+        return res;
+    }
+}
---
+++ algorithms/src/test/java/top/auzqy/_0101_0150/_0118/Solution_0118Test.java
@@ -0,0 +1,15 @@
+package top.auzqy._0101_0150._0118;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution_0118Test {
+
+
+}
