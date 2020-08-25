0caaf02557970f2dd16302153bc65c6e007bc3a9
au <auzhouqingyu@163.com>
2020-08-26 00:37:32 +0800

    leetcode 26 test pass

--- algorithms/src/main/java/top/auzqy/_0001_0050/_0026/Solution_0026.java
+++ algorithms/src/main/java/top/auzqy/_0001_0050/_0026/Solution_0026.java
@@ -2,10 +2,11 @@ package top.auzqy._0001_0050._0026;
 
 /**
  * description:  26. 删除排序数组中的重复项
- *
+ * <p>
  * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/
- *
+ * <p>
  * createTime: 2020/8/25 23:45
+ *
  * @author au
  */
 public class Solution_0026 {
@@ -14,6 +15,16 @@ public class Solution_0026 {
         return solution1_by_au(nums);
     }
 
+    /**
+     * description:
+     * 执行用时：1 ms, 在所有 Java 提交中击败了98.48%的用户
+     * 内存消耗：41.5 MB, 在所有 Java 提交中击败了81.11%的用户
+     *
+     * createTime: 2020/8/26 00:36
+     * @author au
+     * @param nums
+     * @return
+     */
     private int solution1_by_au(int[] nums) {
 
         int count = 0;
@@ -21,12 +32,13 @@ public class Solution_0026 {
         int length = nums.length;
         for (int i = 0; i < length; ) {
             int j = i + 1;
-
             while (j < length && nums[j] == nums[i]) {
                 j++;
             }
             count++;
-            if (j < length && j > k) {
+            if (j >= length) {
+                break;
+            }else if (j >= k) {
                 nums[k++] = nums[j];
                 i = j;
             } else {
--- algorithms/src/test/java/top/auzqy/_0001_0050/_0026/Solution_0026Test.java
+++ algorithms/src/test/java/top/auzqy/_0001_0050/_0026/Solution_0026Test.java
@@ -12,7 +12,9 @@ class Solution_0026Test {
     static Stream<Arguments> intExpectedRes_StringExpectedArr_intArrInput() {
         return Stream.of(
                 Arguments.of(2,"1 2 ",new int[]{1,1,2}),
-                Arguments.of(5,"0 1 2 3 4 ",new int[]{0,0,1,1,1,2,2,3,3,4})
+                Arguments.of(5,"0 1 2 3 4 ",new int[]{0,0,1,1,1,2,2,3,3,4}),
+                Arguments.of(1,"1 ",new int[]{1,1}),
+                Arguments.of(1,"1 ",new int[]{1})
         );
     }
 
