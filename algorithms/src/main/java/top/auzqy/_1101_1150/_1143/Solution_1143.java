060a97c30a4a78eb0456d4f17376d3761ec1baa4
au <auzhouqingyu@163.com>
2020-08-30 00:17:01 +0800

    leetcode 1143 pass

--- algorithms/src/main/java/top/auzqy/_1101_1150/_1143/Solution_1143.java
+++ algorithms/src/main/java/top/auzqy/_1101_1150/_1143/Solution_1143.java
@@ -13,7 +13,31 @@ import java.util.Arrays;
 public class Solution_1143 {
 
     public int longestCommonSubsequence(String text1, String text2) {
-        return solution1_by_au(text1, text2);
+//        return solution1_by_au(text1, text2);
+        return solution2_by_au(text1, text2);
+    }
+
+    private int solution2_by_au(String text1, String text2) {
+        int len1 = text1.length();
+        int len2 = text2.length();
+        int[][] dp = new int[len1 + 1][len2 + 1];
+
+        // int 数组默认值就是 0，所以这两句可以省略
+        // Arrays.fill(dp[0],0);
+
+        for (int i = 1; i < len1 + 1; i++) {
+            // dp[i][0] = 0;
+            for (int j = 1; j < len2 + 1; j++) {
+
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+
+        return dp[len1][len2];
     }
 
     /**
